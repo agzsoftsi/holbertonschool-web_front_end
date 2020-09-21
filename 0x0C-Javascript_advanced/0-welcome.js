0e0d4052f795aee1bbbc484bc60c3dd038c40a79
agzsoftsi <agzsoftsi@gmail.com>
2020-09-21 14:53:34 -0500

    Task 0 Done

---
+++ 0x0C-Javascript_advanced/0-welcome.js
@@ -0,0 +1,21 @@
+ /**
+   * @function welcome - It concatenates two parameters
+   * @param  {String} firstName  Space Delimited sequence of names.
+   * @param  {String} lastName   A period separating the initials.
+   */
+
+   function welcome(firstName, lastName) 
+    {
+       /**
+        * @var {String} fullName - It containts the concatenation of two arguments 
+        */
+        const fullName = firstName + ' ' + lastName;
+       /**
+        * @function displayFullName - It should display an alert with the message
+        */
+        function displayFullName()
+            {
+                alert('Welcome ' + fullName + '!');
+            }
+            displayFullName();
+    }
